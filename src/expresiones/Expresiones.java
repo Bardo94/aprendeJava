2d204e55cb3c5763844bb219f0ee7c943fce3baf
usuario <usuario@usuarioEntDes>
2018-10-03 19:26:32 +0200

    añadida declaración de variables

---
+++ src/expresiones/Expresiones.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expresiones;
+
+/**
+ *
+ * @author usuario
+ */
+public class Expresiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    /**
+     * Método con expresiones para comprobar el resultado.
+     */
+    public static void precedencia() {
+        //Expresiones declaración de las variables de un tipo básico.
+        int expresion1 = 0;
+        int expresion2;
+        int expresion3;
+        int expresion4 = 0;
+
+        //Asigna expresiones a cada una de las variables.
+        //Imprime en la consola el valor de las expresiones.
+        System.out.println("Expresion1: " + expresion1);
+        System.out.println("Expresion1: " + expresion2);
+        System.out.println("Expresion1: " + expresion3);
+        System.out.println("Expresion1: " + expresion4);
+    }
+
+}
