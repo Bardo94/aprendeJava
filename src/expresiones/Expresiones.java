02b342012e98c3557af85c932d3e9792bda6efa0
profesor <profe@usuarioEntDes>
2018-10-03 19:47:23 +0200

    Con la precedencia lista

--- src/expresiones/Expresiones.java
+++ src/expresiones/Expresiones.java
@@ -22,18 +22,8 @@ public class Expresiones {
      * Método con expresiones para comprobar el resultado.
      */
     public static void precedencia() {
-        //Expresiones declaración de las variables de un tipo básico.
-        int expresion1 = 0;
-        int expresion2;
-        int expresion3;
-        int expresion4;
 
-        //Asigna expresiones a cada una de las variables.
-        //Imprime en la consola el valor de las expresiones.
-        System.out.println("Expresion1: " + expresion1);
-        System.out.println("Expresion1: " + expresion2);
-        System.out.println("Expresion1: " + expresion3);
-        System.out.println("Expresion1: " + expresion4);
+
     }
 
 }
