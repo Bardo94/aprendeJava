bd054e5c8be3bbd96aacc85abbeeb1b82b3fd7a0
profesor <profe@usuarioEntDes>
2018-10-03 19:56:02 +0200

    Listo para realizar la práctica

--- src/expresiones/Expresiones.java
+++ src/expresiones/Expresiones.java
@@ -24,16 +24,12 @@ public class Expresiones {
     public static void precedencia() {
         //Expresiones declaración de las variables de un tipo básico.
         int expresion1 = 0;
-        int expresion2;
-        int expresion3;
-        int expresion4;
 
         //Asigna expresiones a cada una de las variables.
+        
         //Imprime en la consola el valor de las expresiones.
         System.out.println("Expresion1: " + expresion1);
-        System.out.println("Expresion1: " + expresion2);
-        System.out.println("Expresion1: " + expresion3);
-        System.out.println("Expresion1: " + expresion4);
+
     }
 
 }
